87233e3aea6ef8cb4952b86647aaf6a83b9537cb
drelleum <mueller8of8@gmail.com>
2015-07-22 15:27:39 -0700

    Added splash screen, a password option, and some more comments

--- LauncherExample/src/com/vnc/android/launcherexample/LauncherActivity.java
+++ LauncherExample/src/com/vnc/android/launcherexample/LauncherActivity.java
@@ -7,17 +7,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+
+import com.iiordanov.bVNC.Constants;
+
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 
 public class LauncherActivity extends ActionBarActivity {
 
-	// Launches the Remote Viewer from bVNC to connect to the given ip address
+	// Launches the Remote Viewer from bVNC to connect to the given ip address with the given password
+	// Make sure to include com.iiordanov.bVNC.Constants if you're going to use them
 	public void launchbVNC(View view) {
-		EditText editText = (EditText)findViewById(R.id.ipText);
+		EditText ipText = (EditText)findViewById(R.id.ipText);
+		EditText pwText = (EditText)findViewById(R.id.pwText);
 		Intent intent = new Intent(this, com.iiordanov.bVNC.RemoteCanvasActivity.class);
-		intent.setData(Uri.parse("vnc://"+editText.getText().toString()+":5900/"));
+		intent.setData(Uri.parse("vnc://"+ipText.getText().toString()+":5900/?"+Constants.PARAM_VNC_PWD+"="+pwText.getText().toString()));
 		try {
 			startActivity(intent);
 		} catch (ActivityNotFoundException e) {
@@ -25,7 +30,7 @@ public class LauncherActivity extends ActionBarActivity {
 		}
 	}
 	
-	// Launches the Remote Viewer from bVNC to connect to localhost
+	// Launches the Remote Viewer from bVNC to connect to localhost with no password
 	public void launchLocalhost(View view) {
 		Intent intent = new Intent(this, com.iiordanov.bVNC.RemoteCanvasActivity.class);
 		intent.setData(Uri.parse("vnc://localhost:5900/"));
@@ -43,10 +48,11 @@ public class LauncherActivity extends ActionBarActivity {
 		 *  Comment this in to automatically start the VNC connection upon
 		 *  creation of this activity. Note the IP is hard-coded at this
 		 *  point, but could easily be fed in from some outside source 
-		 *  or calling intent
+		 *  or calling intent. The Password is optional and can be fed in 
+		 *  the same as the ip address.
 		 *  
 		Intent intent = new Intent(this, com.iiordanov.bVNC.RemoteCanvasActivity.class);
-		intent.setData(Uri.parse("vnc://172.17.161.71:5900/"));
+		intent.setData(Uri.parse("vnc://172.17.161.71:5900/?"+Constants.PARAM_VNC_PWD+"=blabla  "));
 		startActivity(intent);
 		 */
 		setContentView(R.layout.activity_launcher);
---
+++ LauncherExample/src/com/vnc/android/launcherexample/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.vnc.android.launcherexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+	// Splash screen timer
+	private static int SPLASH_TIME_OUT = 3000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		new Handler().postDelayed(new Runnable() {
+			
+			/*
+			 * Showing splash screen witha timer
+			 */
+			
+			@Override
+			public void run() {
+				// This method will be executed once the timer is over
+				// Start your app main activity
+				Intent intent = new Intent(SplashScreen.this, LauncherActivity.class);
+				startActivity(intent);
+				
+				// close this activity
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+	}
+}
